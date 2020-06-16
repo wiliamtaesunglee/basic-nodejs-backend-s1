f2ccfc5b48751401cd7c561c00c75d21a8db3876
wil.i.am <wiliam.tae@gmail.com>
2020-06-16 18:35:49 -0300

    add jest file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { uuid, isUuid } = require('uuidv4')
+const cors = require('cors');
+const { uuid, isUuid } = require('uuidv4');
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const projects = [];
